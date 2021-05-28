f1dec64cb7d7160380d8d20b753d0f8e4ce2bd3f
mh-saeed <saeedkhan111244@gmail.com>
2021-05-28 18:26:04 +0500

    updated

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -26,7 +26,7 @@ const Chat = ({ location }) => {
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    socket = io.connect(ENDPOINT);
+    socket = io.connect(ENDPOINT,{ transports: ['websocket', 'polling', 'flashsocket'] });
 
     setRoom(room);
     setName(name);
